18b20363e497176cca95eed22ddd5a608a9d5182
nitishakumari2745-create <nitishakumari2745@gmail.com>
2026-06-13 22:03:02 +0530

    Add files via upload

---
+++ app (3).py
@@ -0,0 +1,9 @@
+
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+st.title("Instructor Performance and Course Quality Evaluation")
+
+st.write("Dashboard Created Successfully")
